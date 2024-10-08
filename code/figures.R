37d89cc019dc1f7cbf4cde280941efdc9a833e4f
Tanya Strydom <tanya.strydom@icloud.com>
2024-10-08 11:33:52 +0100

    ⛸️ summary figure

---
+++ code/figures.R
@@ -0,0 +1,47 @@
+library(readr)
+library(scales)
+library(tidyverse)
+
+df = list.files(path = "data/processed/", pattern = ".csv", full.names = TRUE) %>% 
+  lapply(read_csv) %>% 
+  bind_rows %>% 
+        select(-c(network, richness)) %>% 
+        # to get the ratio
+        mutate(ratio = top/basal,
+                top = NULL,
+                basal = NULL) %>%
+        pivot_longer(
+            cols = -c(id, model), 
+            names_to = "stat",
+            values_to = "stat_val")  %>% 
+    # standardise names
+        mutate(id = case_when(
+            str_detect(id, "^.*pre.*$") ~ "pre",
+            str_detect(id, "^.*during.*$") ~ "post",
+            str_detect(id, "^.*post.*$") ~ "during",
+            TRUE ~ as.character(id)))
+
+df$id <- ordered(df$id, levels=c("pre", "during", "post"))
+
+ggplot(df,
+        aes(x = factor(`id`), 
+            y = stat_val, 
+            colour = model,
+            group = model)) +
+    geom_line() +
+    geom_point() +
+    facet_wrap(vars(stat),
+                scales = 'free') +
+    scale_size(guide = 'none') +
+    theme_classic() +
+    xlab("time") +
+    ylab("value") +
+    theme(panel.border = element_rect(colour = 'black',
+                                      fill = "#ffffff00"),
+            axis.ticks.x = element_blank())
+
+ggsave("figures/summary.png",
+       width = 9000,
+       height = 5000,
+       units = "px",
+       dpi = 600)
