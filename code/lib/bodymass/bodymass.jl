bbe891b213dbdde64f09cd34ceed9cc0883ba979
Tanya Strydom <tanya.strydom@icloud.com>
2024-10-11 10:06:47 +0100

    :books: documentation

--- code/lib/bodymass/bodymass.jl
+++ code/lib/bodymass/bodymass.jl
@@ -1,6 +1,33 @@
 using DataFrames
 using SpeciesInteractionNetworks
 
+"""
+  bmratio(
+    species::Any,
+    bodymass::Vector{Float64};
+    α::Float64,
+    β::Float64,
+    γ::Float64,
+)
+
+    Impliments the Bodymass-ratio model as per Roht et al. (2010), 
+    however does not include the latent trait variables. α, β, and γ 
+    paramters are set to those used by Yeakel (2014) and so only
+    requires a species list and their bodymass.
+    
+    #### References
+    
+    Rohr, R.P., H. Scherer, P. Kehrli, C. Mazza, and L-F. Bersier. 2010. 
+    “Modeling Food Webs: Exploring Unexplained Structure Using Latent 
+    Traits.” The American Naturalist 176 (2): 170–77. 
+    https://doi.org/10.1086/653667.
+
+    Yeakel, J.D., M.M. Pires, L. Rudolf, N.J. Dominy, P.L. Koch, P.R. 
+    Guimarães, and T. Gross. 2014. “Collapse of an Ecological Network in 
+    Ancient Egypt.” PNAS 111 (40): 14472–77. 
+    https://doi.org/10.1073/pnas.1408471111.
+
+"""
 function bmratio(
     species::Any,
     bodymass::Vector{Float64};
