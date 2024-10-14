5cd6edc2cc0c9eba81116a0a7e3050e73426e8c4
Tanya Strydom <tanya.strydom@icloud.com>
2024-10-14 14:14:36 +0100

    :racehorse: curate extinction traits data

--- code/01_data_clean.R
+++ code/01_data_clean.R
@@ -4,35 +4,35 @@ library(tidyverse)
 trait_files <- list.files(path = "data/raw", pattern = "traits.csv", full.names = TRUE)
 
 for (i in seq_along(trait_files)) {
-
-    df <- read.csv(trait_files[i])   %>%
-   # standardise strings
-   # mutate(species = str_replace_all(species, "[:punct:]", "")) %>% 
-   # then all whitespace
-   mutate(species = str_replace_all(species, " ", "_")) %>%
-        # standardise strings
-        mutate_all(~str_replace_all(., "-", "_"))  %>%
-        # remove for now
-        filter(!(feeding %in% c("primary", "zooplankton")))  %>%
-        # change motiliy class - we can remove this later
-        mutate(motility = case_when( 
-               motility == "nonmotile" ~ "non_motile",
-               TRUE ~ as.character(motility)))  %>%
-        # divorce feeding from size
-        mutate(size = case_when(
-            str_detect(size, "^.*small.*$") ~ "small",
-            str_detect(size, "^.*large.*$") ~ "large",
-            str_detect(size, "^.*medium.*$") ~ "medium",
-            str_detect(size, "^.*tiny.*$") ~ "tiny",
-            species == "Hemigordius baoqingensis" ~ "medium",
-            TRUE ~ as.character(size)),
-            # same for feeding
-            feeding = case_when(
-                        feeding == "microcarnivore" ~ "carnivore",
-                        TRUE ~ as.character(feeding)))
-
-    # write as clean data
-    write.csv(df, str_replace(trait_files[i], "raw.", "clean/trait/"),
+  
+  df <- read.csv(trait_files[i])   %>%
+    # standardise strings
+    # mutate(species = str_replace_all(species, "[:punct:]", "")) %>% 
+    # then all whitespace
+    mutate(species = str_replace_all(species, " ", "_")) %>%
+    # standardise strings
+    mutate_all(~str_replace_all(., "-", "_"))  %>%
+    # remove for now
+    filter(!(feeding %in% c("primary", "zooplankton")))  %>%
+    # change motiliy class - we can remove this later
+    mutate(motility = case_when( 
+      motility == "nonmotile" ~ "non_motile",
+      TRUE ~ as.character(motility)))  %>%
+    # divorce feeding from size
+    mutate(size = case_when(
+      str_detect(size, "^.*small.*$") ~ "small",
+      str_detect(size, "^.*large.*$") ~ "large",
+      str_detect(size, "^.*medium.*$") ~ "medium",
+      str_detect(size, "^.*tiny.*$") ~ "tiny",
+      species == "Hemigordius baoqingensis" ~ "medium",
+      TRUE ~ as.character(size)),
+      # same for feeding
+      feeding = case_when(
+        feeding == "microcarnivore" ~ "carnivore",
+        TRUE ~ as.character(feeding)))
+  
+  # write as clean data
+  write.csv(df, str_replace(trait_files[i], "raw.", "clean/trait/"),
             row.names = FALSE)
 }
 
@@ -41,21 +41,41 @@ for (i in seq_along(trait_files)) {
 size_files <- list.files(path = "data/raw", pattern = "size.csv", full.names = TRUE)
 
 for (i in seq_along(size_files)) {
-   
-   df <- read.csv(size_files[i])  %>%
-   # standardise strings
-   # mutate(species = str_replace_all(species, "[:punct:]", "")) %>% 
-   # then all whitespace
-   mutate(species = str_replace_all(species, " ", "_")) %>%
-   select(species, Height..interpreted., Length..interpreted.)  %>%
-   group_by(species)  %>%
-   summarise(height = mean(Height..interpreted.),
-            length = mean(Length..interpreted.))  %>% 
+  
+  df <- read.csv(size_files[i])  %>%
+    # standardise strings
+    # mutate(species = str_replace_all(species, "[:punct:]", "")) %>% 
+    # then all whitespace
+    mutate(species = str_replace_all(species, " ", "_")) %>%
+    select(species, Height..interpreted., Length..interpreted.)  %>%
+    group_by(species)  %>%
+    summarise(height = mean(Height..interpreted.),
+              length = mean(Length..interpreted.))  %>% 
     reframe(
-        species = species,
-        size = length * height)
-
-    # write as clean data
-    write.csv(df, str_replace(size_files[i], "raw.", "clean/size/"),
+      species = species,
+      size = length * height)
+  
+  # write as clean data
+  write.csv(df, str_replace(size_files[i], "raw.", "clean/size/"),
             row.names = FALSE)
 }
+
+# create simple traits df for extinction sims
+
+list.files(path = "../data/clean/trait", pattern = ".csv", full.names = TRUE) %>% 
+  lapply(read_csv) %>% 
+  bind_rows %>%
+  select(-feeding) %>%
+  mutate(motility = case_when(str_detect(motility, "^.*non_motile.*$") ~ "non_motile",
+                              motility == "slow_moving" ~ "slow",
+                              motility == "fast_moving" ~ "fast",
+                              TRUE ~ as.character(motility)),
+         tiering = case_when(str_detect(tiering, "^.*infaunal.*$") ~ "infaunal",
+                             str_detect(tiering, "^.*epifaunal.*$") ~ "epifaunal",
+                             TRUE ~ as.character(tiering))
+         ) %>%
+  distinct() %>%
+  write.csv(., "../data/clean/extinction/traits.csv",
+            row.names = FALSE)
+
+
