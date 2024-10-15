d225e970594c75076377daab403ff36688a56fe8
Tanya Strydom <tanya.strydom@icloud.com>
2024-10-15 11:39:46 +0100

    :sparkles: add hierarchical extinctions

--- code/06_extinctions.jl
+++ code/06_extinctions.jl
@@ -9,27 +9,19 @@ include("lib/internals.jl")
 import Random
 Random.seed!(66)
 
-# create trait dataframe
-# get the name of all communities
-matrix_names = readdir("../data/clean/trait")
-matrix_names = replace.(matrix_names, ".csv" => "")
-
 # get the traits data
-traits = DataFrame(
-    species = Any[],
-    feeding = Any[],
-    motility = Any[],
-    tiering = Any[],
-    size = Any[],
-)
+traits = DataFrame(CSV.File("../data/clean/extinction/traits.csv"))
 
-for i in eachindex(matrix_names)
-    file_name = matrix_names[i]
-    df = DataFrame(CSV.File.(joinpath("../data/clean/trait", "$file_name.csv"),))
-    append!(traits, df)
-end
-
-traits = unique(traits)
+# specify hierarchies
+hierarchies = [
+    # trait class
+    [:size, :motility, :tiering],
+    # hierarchy for trait class
+    [
+        ["tiny", "small", "medium", "large"],
+        ["non_motile", "facultative", "slow", "fast"],
+        ["infaunal", "epifaunal", "nektonic", "pelagic"]
+    ]]
 
 # Extinction sequence
 
@@ -52,7 +44,7 @@ for i in eachindex(matrix_names)
 
     # now we loop throuhg the different time periods
     for j in ["pre", "during"]
-        # select only the pre extinction community
+        # select only the relevant community
         pre_comm = df[occursin.(j, df.id), :]
         # get post extinction richness
         post_rich = richness(df[occursin.("post", df.id), :network][1])
@@ -75,6 +67,7 @@ for i in eachindex(matrix_names)
         # numeric extinctions
         for k in ["generality", "vulnerability"]
 
+            # turn into function
             f = getfield(Main, Symbol(k))
 
             extinction_list = extinction_sequence(f(pre_comm.network[1]))
@@ -92,6 +85,32 @@ for i in eachindex(matrix_names)
             D[:time] = j
 
             push!(extinction_results, D)
+        end
+
+        # categorical extinctions
+        # note we cant do this with the niche model...
+        if pre_comm.model[1] != "niche"
+            for k in eachindex(hierarchies)
+
+                trait_data = traits[:, [:species, hierarchies[1][k]]]
+                rename!(trait_data, hierarchies[1][k] => :trait)
+    
+                extinction_list = extinction_sequence(hierarchies[2][k], trait_data)
+    
+                # generate extinction sequence
+                extinction_series = extinction(pre_comm.network[1], extinction_list, post_rich)
+    
+                # summarise extinction network
+                D = _network_summary(extinction_series[end])
+    
+                # append additional info
+                D[:model] = pre_comm.model[1]
+                D[:extinction_mechanism] = string(hierarchies[1][k])
+                D[:id] = pre_comm.id[1]
+                D[:time] = j
+    
+                push!(extinction_results, D)
+            end
         end
     end
 end
