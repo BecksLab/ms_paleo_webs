b4bb5565ed9307a394fb8703e0d471680eff6bf4
Tanya Strydom <tanya.strydom@icloud.com>
2024-10-22 10:29:07 +0100

    :racehorse: add pfim metaweb

--- code/03_network_pfim.jl
+++ code/03_network_pfim.jl
@@ -36,3 +36,28 @@ save_object(
     "../data/processed/networks/pfim_networks.jlds",
     topology[:, ["id", "model", "network"]],
 )
+
+# repeat but without downsampling (i.e., metawebs)
+
+for i in eachindex(matrix_names)
+
+    file_name = matrix_names[i]
+    df = DataFrame(CSV.File.(joinpath("../data/clean/trait", "$file_name.csv"),))
+
+    d = model_summary(df, file_name, "pfim", downsample = false)
+
+    d[:model] = "pfim_metaweb"
+    push!(topology, d)
+
+end
+
+# write summaries as .csv
+CSV.write(
+    "../data/processed/topology_pfim_metaweb.csv",
+    topology[:, setdiff(names(topology), ["network"])],
+)
+# write networks as object
+save_object(
+    "../data/processed/networks/pfim_metawebs.jlds",
+    topology[:, ["id", "model", "network"]],
+)
