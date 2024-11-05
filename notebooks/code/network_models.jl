eee3fcd8c7ba1362dcb8115e62c8a962d8db6df8
Tanya Strydom <tanya.strydom@icloud.com>
2024-11-05 14:27:23 +0000

    ♻️ re-calc nz network summaries

--- notebooks/code/network_models.jl
+++ notebooks/code/network_models.jl
@@ -14,15 +14,46 @@ include("../../code/lib/adbm/adbm.jl")
 include("../../code/lib/internals.jl")
 
 # import datasets
-nz_summaries = DataFrame(CSV.File("../notebooks/data/processed/nz_summary.csv"))
-nz_networks = load_object("../notebooks/data/raw/nz_networks.jlds")
-comm_data = DataFrame(CSV.File("../notebooks/data/raw/taxa_dry_weight_abundance.csv"))
+#nz_summaries = DataFrame(CSV.File("../notebooks/data/processed/nz_summary.csv"))
+nz_networks = load_object("notebooks/data/raw/nz_networks.jlds")
+comm_data = DataFrame(CSV.File("notebooks/data/raw/taxa_dry_weight_abundance.csv"))
 rename!(comm_data, :dw => :bodymass)
 rename!(comm_data, Symbol("no.m2") => :abundance)
 rename!(comm_data, :taxa  => :species)
 
-# side quest: find 'producer' species
+# summarise NZ networks
 
+nz_summaries = topo_df();
+
+for i in eachindex(nz_networks)
+
+   d = _network_summary(nz_networks[i].network)
+
+   D = Dict{Symbol,Any}()
+   D[:id] = nz_networks[i].id
+   D[:model] = "NA"
+   D[:richness] = d[:richness]
+   D[:connectance] = d[:connectance]
+   D[:complexity] = d[:complexity]
+   D[:distance] = d[:distance]
+   D[:basal] = d[:basal]
+   D[:top] = d[:top]
+   D[:generality] = d[:generality]
+   D[:vulnerability] = d[:vulnerability]
+   D[:S1] = d[:S1]
+   D[:S2] = d[:S2]
+   D[:S4] = d[:S4]
+   D[:S5] = d[:S5]
+   D[:network] = nz_networks[i].network
+
+    push!(nz_summaries, D)
+end
+
+# write summaries as .csv
+CSV.write("notebooks/data/processed/nz_summary.csv",
+            nz_summaries[:, setdiff(names(nz_summaries), ["model", "network"])])
+
+# side quest: find 'producer' species
 producer = 
     DataFrame(
             species = String15[],
@@ -44,8 +75,6 @@ for i in eachindex(nz_networks)
     producer = vcat(p, producer)
 end
 
-
-
 # master df
 topology = topo_df();
 
@@ -53,7 +82,7 @@ topology = topo_df();
 Co = 0.1;
 
 # number of networks to generate
-n_reps = 100;
+n_reps = 1000;
 
 @showprogress for _ = 1:n_reps
     for i in 1:nrow(nz_summaries)
