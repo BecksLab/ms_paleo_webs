1436190d3c2cc6fbf3c461e59f0192ee504733fa
Tanya Strydom <tanya.strydom@icloud.com>
2024-12-10 10:41:17 +0000

    ✨ trophic species networks

--- code/03_network_pfim.jl
+++ code/03_network_pfim.jl
@@ -167,3 +167,34 @@ save_object(
     "../data/processed/networks/pfim_networks_basal.jlds",
     topology[:, ["id", "model", "network"]],
 )
+
+# trophic species
+
+# get the name of all communities
+matrix_names = readdir("../data/clean/trophic")
+matrix_names = replace.(matrix_names, ".csv" => "")
+
+topology = topo_df();
+
+for i in eachindex(matrix_names)
+
+    file_name = matrix_names[i]
+    df = DataFrame(CSV.File.(joinpath("../data/clean/trophic", "$file_name.csv"),))
+
+    d = model_summary(df, file_name, "pfim", downsample = false)
+
+    d[:model] = "pfim_trophic"
+    push!(topology, d)
+
+end
+
+# write summaries as .csv
+CSV.write(
+    "../data/processed/topology_pfim_trophic.csv",
+    topology[:, setdiff(names(topology), ["network"])],
+)
+# write networks as object
+save_object(
+    "../data/processed/networks/pfim_networks_trophic.jlds",
+    topology[:, ["id", "model", "network"]],
+)
