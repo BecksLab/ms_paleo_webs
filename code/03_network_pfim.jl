1b1046e194cc874027b6a6fadc74d930a0992e29
Tanya Strydom <tanya.strydom@icloud.com>
2025-01-06 13:02:41 +0200

    🐛 filtering of primary node was wrong

--- code/03_network_pfim.jl
+++ code/03_network_pfim.jl
@@ -12,15 +12,20 @@ Random.seed!(66)
 topology = topo_df();
 
 # get the name of all communities
-matrix_names = readdir("../data/clean/trait_minimum")
+matrix_names = readdir("../data/clean/trait_maximal")
 matrix_names = replace.(matrix_names, ".csv" => "")
 
-feeding_rules = DataFrame(CSV.File("../data/clean/feeding_rules/feeding_rules_minimum.csv"))
+# maximal feeding rules
+
+feeding_rules = DataFrame(CSV.File("../data/clean/feeding_rules/feeding_rules_maximal.csv"))
 
 for i in eachindex(matrix_names)
 
     file_name = matrix_names[i]
     df = DataFrame(CSV.File.(joinpath("../data/clean/trait_maximal", "$file_name.csv"),))
+
+    # remove parasites and scavengers
+    filter!(row -> row.species ∉ ["primary"], df)
 
     d = model_summary(df, file_name, "pfim"; feeding_rules = feeding_rules, downsample = false)
 
@@ -40,7 +45,7 @@ save_object(
     topology[:, ["id", "model", "network"]],
 )
 
-# repeat but without downsampling (i.e., metawebs)
+# minimum feeding rules
 
 topology = topo_df();
 
@@ -50,6 +55,9 @@ for i in eachindex(matrix_names)
 
     file_name = matrix_names[i]
     df = DataFrame(CSV.File.(joinpath("../data/clean/trait_minimum", "$file_name.csv"),))
+
+    # remove parasites and scavengers
+    filter!(row -> row.species ∉ ["primary"], df)
 
     d = model_summary(df, file_name, "pfim"; feeding_rules = feeding_rules, downsample = false)
 
@@ -69,7 +77,7 @@ save_object(
     topology[:, ["id", "model", "network"]],
 )
 
-# now we try with numeric size and not categorical
+# numerical size
 
 size_names = readdir("../data/clean/size")
 size_names = replace.(size_names, ".csv" => "")
@@ -119,7 +127,7 @@ for i in eachindex(matrix_names)
     df = DataFrame(CSV.File.(joinpath("../data/clean/trait_minimum", "$file_name.csv"),))
 
     # remove parasites and scavengers
-    filter!(row -> row.feeding ∉ ["parasitic", "scavenger"], df)
+    filter!(row -> row.feeding ∉ ["parasitic", "scavenger", "primary_feeding"], df)
 
     d = model_summary(df, file_name, "pfim"; feeding_rules = feeding_rules, downsample = false)
 
@@ -139,25 +147,22 @@ save_object(
     topology[:, ["id", "model", "network"]],
 )
 
-# repeat but without downsampling (i.e., metawebs) with basal node
+# keep primary (basal) node
 
 topology = topo_df();
+
+# get the name of all communities
+matrix_names = readdir("../data/clean/trait_minimum")
+matrix_names = replace.(matrix_names, ".csv" => "")
+
+feeding_rules = DataFrame(CSV.File("../data/clean/feeding_rules/feeding_rules_minimum.csv"))
 
 for i in eachindex(matrix_names)
 
     file_name = matrix_names[i]
-    df = DataFrame(CSV.File.(joinpath("../data/clean/trait", "$file_name.csv"),))
+    df = DataFrame(CSV.File.(joinpath("../data/clean/trait_minimum", "$file_name.csv"),))
 
-    _basal = Dict{Symbol,Any}()
-    _basal[:species] = "basal"
-    _basal[:feeding] = "producer"
-    _basal[:motility] = "non_motile"
-    _basal[:tiering] = "primary"
-    _basal[:size] = "tiny"
-
-    push!(df, _basal)
-
-    d = model_summary(df, file_name, "pfim", downsample = false)
+    d = model_summary(df, file_name, "pfim"; feeding_rules = feeding_rules, downsample = false)
 
     d[:model] = "pfim_basal"
     push!(topology, d)
