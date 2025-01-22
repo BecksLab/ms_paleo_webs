91bd819062a71abf850c533ff14594e3bb4fd453
Tanya Strydom <tanya.strydom@icloud.com>
2025-01-22 15:21:27 +0200

    🚀 optimise flow

--- code/03_network_pfim.jl
+++ code/03_network_pfim.jl
@@ -9,366 +9,75 @@ include("lib/internals.jl")
 import Random
 Random.seed!(66)
 
-topology = topo_df();
-
 # get the name of all communities
 matrix_names = readdir("../data/clean/trait_maximal")
 matrix_names = replace.(matrix_names, ".csv" => "")
 
-# maximal feeding rules
-feeding_rules = DataFrame(CSV.File("../data/clean/feeding_rules/feeding_rules_maximal.csv"))
+# feeding rules
+max = DataFrame(CSV.File("../data/clean/feeding_rules/feeding_rules_maximal.csv"))
+min = DataFrame(CSV.File("../data/clean/feeding_rules/feeding_rules_minimum.csv"))
 
-for i in eachindex(matrix_names)
+rules = ["maximal" "minimum"
+        max min]
 
-    file_name = matrix_names[i]
-    df = DataFrame(CSV.File.(joinpath("../data/clean/trait_maximal", "$file_name.csv"),))
+# diff datasets
+model_names = ["pfim", "pfim_size", "pfim_basal", "pfim_trophic", "pfim_with_scav"]
 
-    # remove # primary species
-    filter!(row -> row.feeding ∉ ["parasitic", "scavenger", "primary_feeding"], df)
+for j in eachindex(model_names)
 
-    d = model_summary(
-        df,
-        file_name,
-        "pfim";
-        feeding_rules = feeding_rules,
-        downsample = false,
-    )
+    # run PFIM with and without downsampling with both maximal and minimum rules
+    for k in axes(rules, 1)
+        feeding_rules = rules[2,k]
+        feeding = rules[1,k]
 
-    d[:model] = "pfim_maximal"
-    push!(topology, d)
+        topology = topo_df();
+    
+        for i in eachindex(matrix_names)
+            # get data frame
+            file_name = matrix_names[i]
+            df = DataFrame(CSV.File.(joinpath("../data/clean/trait_maximal", "$file_name.csv"),))
+        
+            model = model_names[j]
+        
+            # add/remove nodes based on model/dataset
+            if model == "pfim_with_scav"
+                # remove only basal node
+                filter!(row -> row.feeding ∉ ["primary_feeding"], df)
+            elseif model == "pfim_basal"
+             # remove parasites and scavengers
+                filter!(row -> row.feeding ∉ ["parasitic", "scavenger"], df)
+            else
+                # remove scavenger, parasitic, primary species
+                filter!(row -> row.feeding ∉ ["parasitic", "scavenger", "primary_feeding"], df)
+            end
+            
+            for downsample ∈ [true, false] 
+        
+                d = model_summary(
+                df,
+                file_name,
+                "pfim";
+                feeding_rules = feeding_rules,
+                downsample = downsample,)
 
+                if downsample == true
+                    d[:model] = join([model, "downsample"], "_")
+                    push!(topology, d)
+                else
+                    d[:model] = model
+                    push!(topology, d)
+                end
+            end
+        end
+        # write summaries as .csv
+        CSV.write(
+            join(["../data/processed/topology/", join(["topology", "$model", "$feeding"], "_"), ".csv"]),
+            topology[:, setdiff(names(topology), ["network"])],
+        )
+        # write networks as object
+        save_object(
+            joinpath("../data/processed/networks/", join(["$model", "$feeding", "_networks.jlds"], "_")),
+            topology[:, ["id", "model", "network"]],
+        )
+    end
 end
-
-# write summaries as .csv
-CSV.write(
-    "../data/processed/topology/topology_pfim_maximal.csv",
-    topology[:, setdiff(names(topology), ["network"])],
-)
-# write networks as object
-save_object(
-    "../data/processed/networks/pfim_maximal_networks.jlds",
-    topology[:, ["id", "model", "network"]],
-)
-
-# minimum feeding rules
-
-topology = topo_df();
-
-feeding_rules = DataFrame(CSV.File("../data/clean/feeding_rules/feeding_rules_minimum.csv"))
-
-for i in eachindex(matrix_names)
-
-    file_name = matrix_names[i]
-    df = DataFrame(CSV.File.(joinpath("../data/clean/trait_minimum", "$file_name.csv"),))
-
-    # remove # primary species
-    filter!(row -> row.feeding ∉ ["parasitic", "scavenger", "primary_feeding"], df)
-
-    d = model_summary(
-        df,
-        file_name,
-        "pfim";
-        feeding_rules = feeding_rules,
-        downsample = false,
-    )
-
-    d[:model] = "pfim_minimum"
-    push!(topology, d)
-
-end
-
-# write summaries as .csv
-CSV.write(
-    "../data/processed/topology/topology_pfim_minimum.csv",
-    topology[:, setdiff(names(topology), ["network"])],
-)
-# write networks as object
-save_object(
-    "../data/processed/networks/pfim_minimum_networks.jlds",
-    topology[:, ["id", "model", "network"]],
-)
-
-# numerical size
-
-size_names = readdir("../data/clean/size")
-size_names = replace.(size_names, ".csv" => "")
-
-topology = topo_df();
-
-for i in eachindex(matrix_names)
-
-    file_name = matrix_names[i]
-    df = DataFrame(CSV.File.(joinpath("../data/clean/trait_minimum", "$file_name.csv"),))
-
-    # remove primary node
-    filter!(row -> row.feeding ∉ ["parasitic", "scavenger", "primary_feeding"], df)
-
-    size_file = size_names[i]
-    bodymass = DataFrame(CSV.File.(joinpath("../data/clean/size", "$size_file.csv"),))
-
-    select!(df, Not([:size]))
-    # replace with continuous
-    df = innerjoin(df, bodymass, on = :species)
-
-    d = model_summary(
-        df,
-        file_name,
-        "pfim";
-        feeding_rules = feeding_rules,
-        downsample = false,
-    )
-
-    d[:model] = "pfim_size"
-    push!(topology, d)
-
-end
-
-# write summaries as .csv
-CSV.write(
-    "../data/processed/topology/topology_pfim_size.csv",
-    topology[:, setdiff(names(topology), ["network"])],
-)
-# write networks as object
-save_object(
-    "../data/processed/networks/pfim_networks_size.jlds",
-    topology[:, ["id", "model", "network"]],
-)
-
-# remove scavengers and parasites
-
-topology = topo_df();
-
-feeding_rules = DataFrame(CSV.File("../data/clean/feeding_rules/feeding_rules_noscavs.csv"))
-
-for i in eachindex(matrix_names)
-
-    file_name = matrix_names[i]
-    df = DataFrame(CSV.File.(joinpath("../data/clean/trait_minimum", "$file_name.csv"),))
-
-    # keep parasites and scavengers
-    filter!(row -> row.feeding ∉ ["primary_feeding"], df)
-
-    d = model_summary(
-        df,
-        file_name,
-        "pfim";
-        feeding_rules = feeding_rules,
-        downsample = false,
-    )
-
-    d[:model] = "pfim_with_scav"
-    push!(topology, d)
-
-end
-
-# write summaries as .csv
-CSV.write(
-    "../data/processed/topology/topology_pfim_with_scav.csv",
-    topology[:, setdiff(names(topology), ["network"])],
-)
-# write networks as object
-save_object(
-    "../data/processed/networks/pfim_networks_with_scav.jlds",
-    topology[:, ["id", "model", "network"]],
-)
-
-# keep primary (basal) node
-
-topology = topo_df();
-
-# get the name of all communities
-matrix_names = readdir("../data/clean/trait_minimum")
-matrix_names = replace.(matrix_names, ".csv" => "")
-
-feeding_rules = DataFrame(CSV.File("../data/clean/feeding_rules/feeding_rules_minimum.csv"))
-
-for i in eachindex(matrix_names)
-
-    file_name = matrix_names[i]
-    df = DataFrame(CSV.File.(joinpath("../data/clean/trait_minimum", "$file_name.csv"),))
-
-    # remove parasites and scavengers
-    filter!(row -> row.feeding ∉ ["parasitic", "scavenger"], df)
-
-    d = model_summary(
-        df,
-        file_name,
-        "pfim";
-        feeding_rules = feeding_rules,
-        downsample = false,
-    )
-
-    d[:model] = "pfim_basal"
-    push!(topology, d)
-
-end
-
-# write summaries as .csv
-CSV.write(
-    "../data/processed/topology/topology_pfim_basal.csv",
-    topology[:, setdiff(names(topology), ["network"])],
-)
-# write networks as object
-save_object(
-    "../data/processed/networks/pfim_networks_basal.jlds",
-    topology[:, ["id", "model", "network"]],
-)
-
-# trophic species - minimum rules
-
-# get the name of all communities
-matrix_names = readdir("../data/clean/trophic_minimum")
-matrix_names = replace.(matrix_names, ".csv" => "")
-
-topology = topo_df();
-
-for i in eachindex(matrix_names)
-
-    file_name = matrix_names[i]
-    df = DataFrame(CSV.File.(joinpath("../data/clean/trophic_minimum", "$file_name.csv"),))
-
-    # remove parasites and scavengers
-    filter!(row -> row.feeding ∉ ["parasitic", "scavenger", "primary_feeding"], df)
-
-    d = model_summary(
-        df,
-        file_name,
-        "pfim";
-        feeding_rules = feeding_rules,
-        downsample = false,
-    )
-
-    d[:model] = "pfim_trophic_minimum"
-    push!(topology, d)
-
-end
-
-# write summaries as .csv
-CSV.write(
-    "../data/processed/topology/topology_pfim_trophic_minimum.csv",
-    topology[:, setdiff(names(topology), ["network"])],
-)
-# write networks as object
-save_object(
-    "../data/processed/networks/pfim_networks_trophic_minimum.jlds",
-    topology[:, ["id", "model", "network"]],
-)
-
-# trophic species - maximal rules
-
-# get the name of all communities
-matrix_names = readdir("../data/clean/trophic_maximal")
-matrix_names = replace.(matrix_names, ".csv" => "")
-
-# maximal feeding rules
-feeding_rules = DataFrame(CSV.File("../data/clean/feeding_rules/feeding_rules_maximal.csv"))
-
-topology = topo_df();
-
-for i in eachindex(matrix_names)
-
-    file_name = matrix_names[i]
-    df = DataFrame(CSV.File.(joinpath("../data/clean/trophic_maximal", "$file_name.csv"),))
-
-    # remove parasites and scavengers
-    filter!(row -> row.feeding ∉ ["parasitic", "scavenger", "primary_feeding"], df)
-
-    d = model_summary(
-        df,
-        file_name,
-        "pfim";
-        feeding_rules = feeding_rules,
-        downsample = false,
-    )
-
-    d[:model] = "pfim_trophic_maximal"
-    push!(topology, d)
-
-end
-
-# write summaries as .csv
-CSV.write(
-    "../data/processed/topology/topology_pfim_trophic_maximal.csv",
-    topology[:, setdiff(names(topology), ["network"])],
-)
-# write networks as object
-save_object(
-    "../data/processed/networks/pfim_networks_trophic_maximal.jlds",
-    topology[:, ["id", "model", "network"]],
-)
-
-# downsample minimum
-
-topology = topo_df();
-
-feeding_rules = DataFrame(CSV.File("../data/clean/feeding_rules/feeding_rules_minimum.csv"))
-
-for i in eachindex(matrix_names)
-
-    file_name = matrix_names[i]
-    df = DataFrame(CSV.File.(joinpath("../data/clean/trait_minimum", "$file_name.csv"),))
-
-    # remove parasites and scavengers
-    filter!(row -> row.feeding ∉ ["parasitic", "scavenger", "primary_feeding"], df)
-
-    d = model_summary(
-        df,
-        file_name,
-        "pfim";
-        feeding_rules = feeding_rules,
-        downsample = true,
-    )
-
-    d[:model] = "pfim_minimum_downsample"
-    push!(topology, d)
-
-end
-
-# write summaries as .csv
-CSV.write(
-    "../data/processed/topology/topology_pfim_minimum_downsample.csv",
-    topology[:, setdiff(names(topology), ["network"])],
-)
-# write networks as object
-save_object(
-    "../data/processed/networks/pfim_minimum_downsample_networks.jlds",
-    topology[:, ["id", "model", "network"]],
-)
-
-# downsample maximal
-
-topology = topo_df();
-
-feeding_rules = DataFrame(CSV.File("../data/clean/feeding_rules/feeding_rules_maximal.csv"))
-
-for i in eachindex(matrix_names)
-
-    file_name = matrix_names[i]
-    df = DataFrame(CSV.File.(joinpath("../data/clean/trait_maximal", "$file_name.csv"),))
-
-    # remove parasites and scavengers
-    filter!(row -> row.feeding ∉ ["parasitic", "scavenger", "primary_feeding"], df)
-
-    d = model_summary(
-        df,
-        file_name,
-        "pfim";
-        feeding_rules = feeding_rules,
-        downsample = true,
-    )
-
-    d[:model] = "pfim_maximal_downsample"
-    push!(topology, d)
-
-end
-
-# write summaries as .csv
-CSV.write(
-    "../data/processed/topology/topology_pfim_maximal_downsample.csv",
-    topology[:, setdiff(names(topology), ["network"])],
-)
-# write networks as object
-save_object(
-    "../data/processed/networks/pfim_maximal_downsample_networks.jlds",
-    topology[:, ["id", "model", "network"]],
-)
