6e9b8182c94f4b49c5b6e075c5a0b1883be14568
Tanya Strydom <tanya.strydom@icloud.com>
2025-04-11 14:37:17 +0100

    ✨ add reps for extinctions

--- code/03_extinctions.jl
+++ code/03_extinctions.jl
@@ -41,92 +41,56 @@ extinction_results = DataFrame(
     extinction_seq = Any[],
 );
 
-# Extinction sequence
-
+# Modify datasets
 df = CSV.read("data/raw/G1_Guilds.csv", DataFrame)
-
 select!(
     df,
     [:Guild, :motility_simple, :tiering_simple, :feeding_simple, :size, :calcification],
 )
-
 rename!(df, :Guild => :species)
 rename!(df, :motility_simple => :motility)
 rename!(df, :tiering_simple => :tiering)
 rename!(df, :feeding_simple => :feeding)
 
+# number of extinction sims
+ext_reps = 10
+
 for j = 1:nrow(networks)
 
-    # select correct network
-    N = networks.network[j]
+    for l = 1:ext_reps
 
-    N = add_basal(N)
+        # select correct network
+        N = networks.network[j]
 
-    # random extinction
-    extinction_series = extinction(N; protect = :basal)
+        N = add_basal(N)
 
-    # send results to data frame
-    d = Dict{Symbol,Any}(
-        :model => networks.model[j],
-        :extinction_mechanism => "random",
-        :n_rep => networks.n_rep[j],
-        :extinction_seq => extinction_series,
-    )
+        # random extinction
+        extinction_series = extinction(N; protect = :basal)
 
-    push!(extinction_results, d)
+        # send results to data frame
+        d = Dict{Symbol,Any}(
+            :model => networks.model[j],
+            :extinction_mechanism => "random",
+            :n_rep => networks.n_rep[j],
+            :extinction_seq => extinction_series,
+        )
 
-    # need to simulate for both 'orders' of traits/hierarchies
-    # where true is descending and false is ascending
-    for descending in [true, false]
+        push!(extinction_results, d)
 
-        # numeric extinctions
-        for k in ["generality", "vulnerability"]
+        # need to simulate for both 'orders' of traits/hierarchies
+        # where true is descending and false is ascending
+        for descending in [true, false]
 
-            extinction_series = extinction(N, k, descending)
+            # numeric extinctions
+            for k in ["generality", "vulnerability"]
 
-            # send results to data frame
-            d = Dict{Symbol,Any}(
-                :model => networks.model[j],
-                :extinction_mechanism => join(
-                    [string(k), ifelse(descending, "descending", "ascending")],
-                    "_",
-                ),
-                :n_rep => networks.n_rep[j],
-                :extinction_seq => extinction_series,
-            )
-
-            push!(extinction_results, d)
-
-        end
-
-        # categorical extinctions
-        # note we can't do this with the niche model...
-        if networks.model[j] ∉ ["niche"]
-            for k in eachindex(hierarchies[1])
-
-                # select the correct traits matrix depending on nodes (species vs trophic)
-                trait_data = df[:, [:species, hierarchies[1][k]]]
-                rename!(trait_data, hierarchies[1][k] => :trait)
-
-                # only include species that have the desired traits
-                filter!(:trait => x -> x ∈ hierarchies[2][k], trait_data)
-
-                extinction_list = extinctionsequence(
-                    hierarchies[2][k],
-                    trait_data;
-                    descending = descending,
-                )
-                # generate extinction sequence
-                extinction_series = extinction(N, extinction_list; protect = :basal)
+                extinction_series = extinction(N, k, descending)
 
                 # send results to data frame
                 d = Dict{Symbol,Any}(
                     :model => networks.model[j],
                     :extinction_mechanism => join(
-                        [
-                            string(hierarchies[1][k]),
-                            ifelse(descending, "descending", "ascending"),
-                        ],
+                        [string(k), ifelse(descending, "descending", "ascending")],
                         "_",
                     ),
                     :n_rep => networks.n_rep[j],
@@ -134,6 +98,45 @@ for j = 1:nrow(networks)
                 )
 
                 push!(extinction_results, d)
+
+            end
+
+            # categorical extinctions
+            # note we can't do this with the niche model...
+            if networks.model[j] ∉ ["niche"]
+                for k in eachindex(hierarchies[1])
+
+                    # select the correct traits matrix depending on nodes (species vs trophic)
+                    trait_data = df[:, [:species, hierarchies[1][k]]]
+                    rename!(trait_data, hierarchies[1][k] => :trait)
+
+                    # only include species that have the desired traits
+                    filter!(:trait => x -> x ∈ hierarchies[2][k], trait_data)
+
+                    extinction_list = extinctionsequence(
+                        hierarchies[2][k],
+                        trait_data;
+                        descending = descending,
+                    )
+                    # generate extinction sequence
+                    extinction_series = extinction(N, extinction_list; protect = :basal)
+
+                    # send results to data frame
+                    d = Dict{Symbol,Any}(
+                        :model => networks.model[j],
+                        :extinction_mechanism => join(
+                            [
+                                string(hierarchies[1][k]),
+                                ifelse(descending, "descending", "ascending"),
+                            ],
+                            "_",
+                        ),
+                        :n_rep => networks.n_rep[j],
+                        :extinction_seq => extinction_series,
+                    )
+
+                    push!(extinction_results, d)
+                end
             end
         end
     end
