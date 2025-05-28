8f32bdd4c88f3d9395eb89f386e4339ad889582b
Tanya Strydom <tanya.strydom@icloud.com>
2025-05-28 11:34:59 +0100

    🐎 optimise beta-div analysis

--- code/05_betadiv.jl
+++ code/05_betadiv.jl
@@ -7,6 +7,7 @@ using SpeciesInteractionNetworks
 include("lib/internals.jl")
 
 β_div = DataFrame(
+    time = Any[],
     left = String[],
     right = String[],
     β_int = Int64[],
@@ -18,27 +19,33 @@ include("lib/internals.jl")
 # same comment RE elegance
 networks = load_object("data/processed/networks.jlds")
 
-# this can for sure be done more elegantly but brute force for now...
-# filter!(!=("pfim_networks_basal.jlds"), matrix_names)
+# get the different communities
+time_groups = unique(networks.time)
 
-networks.uniq_id = map(join,zip(networks.model,networks.time,networks.n_rep))
+for i in eachindex(time_groups)
 
-# for each combination of datasets
-for (x, y) in combinations(unique(networks[:, :uniq_id]), 2)
-    U = networks[occursin.(x, networks.uniq_id), :network][1]
-    V = networks[occursin.(y, networks.uniq_id), :network][1]
-    β_spp = SpeciesInteractionNetworks.betadiversity(βS, U, V)
-    β_int = SpeciesInteractionNetworks.betadiversity(βWN, U, V)
-    # collate
-    b = Dict{Symbol,Any}()
-    b[:left] = x
-    b[:right] = y
-    b[:β_int] = β_int.shared
-    b[:β_spp] = β_spp.shared
-    b[:links_left] = links(U)
-    b[:links_right] = links(V)
-    # send to results
-    push!(β_div, b)
+    df = filter(:time => x -> x == time_groups[i], networks)
+    # make unique ID for making combos
+    df.uniq_id = map(join,zip(df.model, df.n_rep))
+
+    # for each combination of datasets
+    for (x, y) in combinations(unique(df[:, :uniq_id]), 2)
+        U = df[occursin.(x, df.uniq_id), :network][1]
+        V = df[occursin.(y, df.uniq_id), :network][1]
+        β_spp = SpeciesInteractionNetworks.betadiversity(βS, U, V)
+        β_int = SpeciesInteractionNetworks.betadiversity(βWN, U, V)
+        # collate
+        b = Dict{Symbol,Any}()
+        b[:time] = time_groups[i]
+        b[:left] = x
+        b[:right] = y
+        b[:β_int] = β_int.shared
+        b[:β_spp] = β_spp.shared
+        b[:links_left] = links(U)
+        b[:links_right] = links(V)
+        # send to results
+        push!(β_div, b)
+    end   
 end
 
 # write outputs as .csv
