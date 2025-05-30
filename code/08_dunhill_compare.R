ec673c651cb5d67beddfc6633e309adec01fd17c
Tanya Strydom <tanya.strydom@icloud.com>
2025-05-30 13:24:28 +0100

    🐣 first pass on recreating Dunhill

---
+++ code/08_dunhill_compare.R
@@ -0,0 +1,119 @@
+# libraries
+library(here)
+library(patchwork)
+library(tidyverse)
+
+# set path to code sub dir
+setwd(here("code"))
+
+# import simulated data
+
+df <- read_csv("../data/processed/topology.csv") %>%
+  #mutate(across(matches("S[[:digit:]]"), log)) %>%
+  select(-c(distance, redundancy, complexity, diameter, n_rep)) %>%
+  pivot_longer(
+    cols = -c(model, time),
+    names_to = "stat",
+    values_to = "stat_val") %>%
+  # get mean values
+  group_by(model, time, stat) %>%
+  summarise(mean = mean(stat_val)) %>%
+  ungroup() %>%
+  # standardise names
+  mutate(stat = case_when(stat == "S1" ~ "No. of linear chains",
+                          stat == "S2" ~ "No. of omnivory motifs",
+                          stat == "S5" ~ "No. of apparent competition motifs",
+                          stat == "S4" ~ "No. of direct competition motifs",
+                          .default = as.character(stat))) %>%
+  mutate(level = case_when(stat %in% c("richness", "connectance", "generality", "vulnerability") ~ "struct",
+                           .default = "motif"),
+         model = factor(model, ordered = TRUE, 
+                        levels = c("niche", "random", "adbm", "lmatrix", "pfim", "bodymassratio")),
+         time = str_extract(time, "\\d+"))
+
+struct <- ggplot(df %>% 
+                   filter(level == "struct"),
+                 aes(x = time,
+                     y = mean,
+                     colour = model,
+                     group = model)) +
+  geom_line() +
+  facet_wrap(vars(stat),
+             scales = 'free',
+             ncol = 1) +
+  scale_size(guide = 'none') +
+  theme_classic() +
+  xlab("time") +
+  ylab("value") +
+  coord_cartesian(clip = "off") +
+  scale_colour_brewer(palette = "Dark2") +
+  theme(panel.border = element_rect(colour = 'black',
+                                    fill = "#ffffff00"),
+        axis.ticks.x = element_blank())
+
+motif <- ggplot(df %>% 
+                  filter(level == "motif"),
+                aes(x = time,
+                    y = mean,
+                    colour = model,
+                    group = model)) +
+  geom_line() +
+  facet_wrap(vars(stat),
+             scales = 'free',
+             ncol = 1) +
+  scale_size(guide = 'none') +
+  theme_classic() +
+  xlab("time") +
+  ylab("value") +
+  coord_cartesian(clip = "off") +
+  scale_colour_brewer(palette = "Dark2") +
+  theme(panel.border = element_rect(colour = 'black',
+                                    fill = "#ffffff00"),
+        axis.ticks.x = element_blank())
+
+# means abs differences between real and extinction simulations
+
+df <- read_csv("../data/processed/extinction_topology.csv") %>%
+  select(-c(r50, rep, distance, redundancy, complexity, diameter, S1, S2, S4, S5, resilience)) %>%
+  pivot_longer(
+    cols = -c(model, extinction_mechanism, n_rep),
+    names_to = "stat",
+    values_to = "sim_val") %>%
+  full_join(.,
+            read_csv("../data/processed/topology.csv") %>%
+              filter(time == "G2") %>%
+              select(-c(distance, redundancy, complexity, diameter, time, S1, S2, S4, S5)) %>%
+              pivot_longer(
+                cols = -c(model, n_rep),
+                names_to = "stat",
+                values_to = "real_val")) %>%
+  mutate(diff = real_val - sim_val) %>%
+  group_by(model, extinction_mechanism) %>%
+  summarise(mean_diff = abs(mean(diff, na.rm = TRUE))) %>%
+  mutate(model = factor(model, ordered = TRUE, 
+                        levels = c("niche", "random", "adbm", "lmatrix", "pfim", "bodymassratio")))
+
+mean_diff <- ggplot(df,
+                    aes(y = extinction_mechanism,
+                        x = mean_diff,
+                        fill = model)) +
+  geom_bar(stat="identity", position=position_dodge()) +
+  coord_cartesian(clip = "off") +
+  theme_classic() +
+  scale_fill_brewer(palette = "Dark2") +
+  theme(panel.border = element_rect(colour = 'black',
+                                    fill = "#ffffff00"),
+        axis.ticks.x = element_blank(),
+        legend.position = 'none')
+
+
+(struct) / (mean_diff + motif)  +
+  plot_layout(guides = 'collect') +
+  plot_annotation(tag_levels = 'A')
+
+ggsave("../figures/dunhill_comp.png",
+       width = 5000,
+       height = 7000,
+       units = "px",
+       dpi = 600)
+
