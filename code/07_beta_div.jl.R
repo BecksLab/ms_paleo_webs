ce7954707cb747e5c6e44cafb8cf789a4ef1546e
Tanya Strydom <tanya.strydom@icloud.com>
2025-06-06 14:23:49 +0100

    🐣 experimenting with β-div plotting

---
+++ code/07_beta_div.jl.R
@@ -0,0 +1,51 @@
+
+# librares
+library(tidyverse)
+
+# set path to code sub dir
+setwd(here("code"))
+
+# import simulated data
+
+df <- read_csv("../data/processed/beta_div.csv") %>%
+  # remove all instances where niche model is present (not a fair comparison?)
+  filter(!str_detect(left, "niche")) %>%
+  filter(!str_detect(right, "niche")) %>%
+  filter(!str_detect(right, "random")) %>%
+  filter(!str_detect(left, "random")) %>%
+  mutate(combo = paste0(str_extract(left, "[a-z]+"), "_", str_extract(right, "[a-z]+"))) %>%
+  mutate(combo = case_when(combo == "bodymassratio_adbm" ~ "adbm_bodymassratio",
+                           combo == "lmatrix_adbm" ~ "adbm_lmatrix",
+                           combo == "pfim_adbm" ~ "adbm_pfim",
+                           combo == "pfim_bodymassratio" ~ "bodymassratio_pfim",
+                           combo == "random_bodymassratio" ~ "bodymassratio_random",
+                           combo == "pfim_lmatrix" ~ "lmatrix_pfim",
+                           combo == "bodymassratio_lmatrix" ~ "lmatrix_bodymassratio",
+                           .default = as.character(combo))) %>%
+  mutate(shared_pct = β_int_shared/((links_left-β_int_shared) + (links_right-β_int_shared) + β_int_shared),
+         model_combo = case_when(combo %in% c("pfim_pfim", "lmatrix_lmatrix", "bodymassratio_bodymassratio",
+                                              "adbm_adbm") ~ "Within model",
+                                 .default = "Between model"))
+
+ggplot(df,
+       aes(x = combo,
+           y = shared_pct*100,
+           colour = time)) + 
+  geom_boxplot(position = position_dodge(1),
+               outliers = FALSE,
+               fill = NA) +
+  facet_wrap(vars(model_combo),
+             scales = 'free_x',
+             ncol = 2) +
+  xlab(NULL) +
+  ylab("% interactions shared") +
+  coord_cartesian(clip = "off") +
+  theme(panel.border = element_rect(colour = 'black',
+                                    fill = "#ffffff00"),
+        axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+ggsave("../figures/beta_div.png",
+       width = 5000,
+       height = 4000,
+       units = "px",
+       dpi = 600)
