73a34f72dcd1771b67c4ecc1076d78ae354f3758
Tanya Strydom <tanya.strydom@icloud.com>
2025-06-13 13:15:55 +0100

    🐣 experimenting with downsampling

---
+++ code/S1_pfim_compare.R
@@ -0,0 +1,49 @@
+library(tidyverse)
+
+# set path to code sub dir
+setwd(here("code"))
+
+# import data
+metaweb <- read_csv("../data/processed/topology.csv") %>%
+  filter(model == "pfim_metaweb") %>%
+  select(-c(distance, n_rep)) %>%
+  # remove metaweb pfims
+  pivot_longer(
+    cols = -c(time, model),
+    names_to = "stat",
+    values_to = "stat_val") %>%
+  group_by(time, stat) %>%
+  summarise(mean = mean(stat_val, na.rm = TRUE))
+
+read_csv("../data/dunhill/topology.csv") %>%
+  select(-c(distance, n_rep)) %>%
+  # remove metaweb pfims
+  pivot_longer(
+    cols = -c(y_val, time),
+    names_to = "stat",
+    values_to = "stat_val") %>%
+  group_by(time, y_val, stat) %>% 
+  summarise(mean = mean(stat_val, na.rm = TRUE)) %>%
+  ggplot(.,
+         aes(y = mean,
+             x = y_val,
+             group = time,
+             colour = time)) +
+  geom_vline(xintercept = 2.5,
+             alpha = 0.2) +
+  geom_line() +
+  geom_hline(data = metaweb,
+             aes(yintercept = mean,
+                 colour = time),
+             linetype = 3) +
+  facet_wrap(vars(stat),
+             scales = 'free',
+             ncol = 2) +
+  scale_size(guide = 'none') +
+  xlab("time") +
+  ylab("value") +
+  coord_cartesian(clip = "off") +
+  theme_classic() +
+  theme(panel.border = element_rect(colour = 'black',
+                                    fill = "#ffffff00"),
+        axis.ticks.x = element_blank())
--- code/S1_pfim_compare.jl
+++ code/S1_pfim_compare.jl
@@ -58,3 +58,71 @@ for j in eachindex(dunhill_comms)
     d_julia = _network_summary(N_julia)
     
 end
+
+# set seed
+import Random
+Random.seed!(66)
+
+# get the name of all communities
+matrix_names = readdir("data/raw")
+# select only species datasets
+matrix_names = matrix_names[occursin.(r"^.*Guilds.*$", matrix_names)]
+
+# feeding rules
+feeding_rules = DataFrame(CSV.File("data/raw/feeding_rules.csv"))
+
+# number of network reps
+n_reps = 100
+y_val = collect(1.0:0.2:10.0)
+
+topology = DataFrame(
+    time = Any[],
+    n_rep = Any[],
+    y_val = Any[],
+    richness = Int64[],
+    connectance = Float64[],
+    diameter = Int64[],
+    complexity = Float64[],
+    distance = Float64[],
+    generality = Float64[],
+    vulnerability = Float64[],
+    redundancy = Float64[],
+    S1 = Float64[],
+    S2 = Float64[],
+    S4 = Float64[],
+    S5 = Float64[],
+);
+
+for j = 1:n_reps
+    
+        for i in eachindex(matrix_names)
+    
+            file_name = matrix_names[i]
+            # get relevant info from slug
+            str_cats = split(file_name, r"_")
+    
+            # import data frame
+            df = DataFrame(CSV.File.(joinpath("data/raw/", "$file_name")))
+            select!(df, [:Guild, :motility, :tiering, :feeding, :size])
+            rename!(df, :Guild => :species)
+    
+            # remove BASAL_NODE for now...
+            filter!(:species => x -> x != "BASAL_NODE", df)
+    
+        for k in eachindex(y_val)
+
+           N = pfim.PFIM(df, feeding_rules; y = y_val[k], downsample = true)
+
+           d = _network_summary(N)
+           
+           d[:time] = str_cats[1]
+           d[:n_rep] = j
+           d[:y_val] = y_val[k]
+
+            push!(topology, d)
+        end
+    end
+end
+
+# write summaries as .csv
+CSV.write("data/dunhill/topology.csv", topology)
