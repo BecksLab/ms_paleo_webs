551b62a2bd03a8e5bf3613f51e30fd209b66d21f
Tanya Strydom <tanya.strydom@icloud.com>
2025-11-04 15:37:16 +0000

    ♻️ rename

--- code/04_extinction_analysis.jl
+++ code/04_extinction_analysis.jl
@@ -21,9 +21,17 @@ networks = load_object("../data/processed/networks.jlds")
 # we only care about the post extinction community
 filter!(:time => x -> x == "G2", networks)
 
-# find richness of post extinction community
-df = CSV.read("../data/raw/G2_Guilds.csv", DataFrame)
-post_rich = nrow(df)
+# find richness of post extinction community (but also only species shared between G1 and G2)
+post_df = CSV.read("../data/raw/G2_Guilds.csv", DataFrame)
+pre_df = CSV.read("../data/raw/G1_Guilds.csv", DataFrame)
+
+# this is the list of species shared
+shared_spp = intersect(post_df.Guild, pre_df.Guild)
+# this is the list of species that entered the community post extinction
+spp_originated = setdiff(post_df.Guild, pre_df.Guild)
+
+# this is the 'target richness' of the extinction sequence
+post_rich = length(shared_spp)
 
 topology = DataFrame(
     model = String[],
@@ -53,6 +61,17 @@ tss = DataFrame(
     tss = Float64[],
 )
 
+β_div = DataFrame(
+    model = String[],
+    extinction_mechanism = Any[],
+    n_rep = Any[],
+    β_int_shared = Int64[],
+    β_int_all = Int64[],
+    β_spp = Int64[],
+    links_left = Int64[],
+    links_right = Int64[],
+)
+
 @showprogress "Getting topology" for i = 1:nrow(extinctions)
 
     _ext_seq = extinctions.extinction_seq[i]
@@ -66,13 +85,23 @@ tss = DataFrame(
 
             # tss score
 
+            # get the real network
             N_real = filter(
                 [:model, :n_rep] =>
                     (x, y) -> x == extinctions.model[i] && y == extinctions.n_rep[i],
                 networks,
             )
 
-            _tss = TSS(N_real.network[1], _ext_seq[net_ind])
+            real_net = add_basal(N_real.network[1])
+            # subset so we remove species that originated
+            # get spp in real and filter that list against the originated spp list
+            real_spp = species(real_net)
+            filter!(x -> x ∉ Symbol.(spp_originated), real_spp)
+
+            real_N = subgraph(real_net, real_spp)
+            sim_N = _ext_seq[net_ind]
+
+            _tss = TSS(real_N, sim_N, _ext_seq[1])
 
             t = Dict{Symbol,Any}(
                 :model => extinctions.model[i],
@@ -93,6 +122,25 @@ tss = DataFrame(
             d[:rep] = i
 
             push!(topology, d)
+
+            # beta div - as an alternative to tss
+            β_spp = SpeciesInteractionNetworks.betadiversity(βS, real_N, sim_N)
+            β_int_all = SpeciesInteractionNetworks.betadiversity(βWN, real_N, sim_N)
+            β_int_shared = SpeciesInteractionNetworks.betadiversity(βOS, real_N, sim_N)
+            # collate
+            b = Dict{Symbol,Any}()
+            b[:model] = extinctions.model[i]
+            b[:extinction_mechanism] = extinctions.extinction_mechanism[i]
+            b[:n_rep] = extinctions.n_rep[i]
+            b[:β_int_all] = β_int_all.shared
+            b[:β_int_shared] = β_int_shared.shared
+            b[:β_spp] = β_spp.shared
+            b[:links_left] = links(real_N)
+            b[:links_right] = links(sim_N)
+
+            # send to results
+            push!(β_div, b)
+
         end
     end
 end
@@ -100,6 +148,8 @@ end
 # write summaries as .csv
 CSV.write("../data/processed/extinction_topology.csv", topology)
 CSV.write("../data/processed/extinction_tss.csv", tss)
+CSV.write("../data/processed/extinction_betadiv.csv", β_div)
+
 
 # robustness curves
 
@@ -108,7 +158,7 @@ networks = load_object("../data/processed/networks.jlds")
 # we only care about the metaweb pfim for now
 filter!(:model => x -> x == "pfim_metaweb", networks)
 
-# here we want to generate robustness curves from teh spread 1:99
+# here we want to generate robustness curves from the spread 1:99
 spread = collect(1:1:99)
 
 n_reps = 500
