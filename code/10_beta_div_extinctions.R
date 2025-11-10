f1774a88096c1b826b9dfc175b5832da29951b9c
Tanya Strydom <tanya.strydom@icloud.com>
2025-11-10 09:11:18 +0000

    ✨ turnover figures

--- code/10_beta_div_extinctions.R
+++ code/10_beta_div_extinctions.R
@@ -1,6 +1,7 @@
 # libraries
 library(ggtext)
 library(here)
+library(patchwork)
 library(tidyverse)
 
 # set path to code sub dir
@@ -22,22 +23,78 @@ df <- read_csv("../data/processed/extinction_betadiv.csv") %>%
                        values_to = "β_div")) %>%
   # get mean (for now might be worth looking at sd as well...)
   group_by(model, extinction_mechanism, β_type) %>%
-  summarise(mean = mean(β_div, na.rm = TRUE)) 
-  
+  summarise(mean = mean(β_div, na.rm = TRUE)) %>%
+  mutate(β_type = case_when(β_type == "βWN" ~ "βWN - dissim of all intxns",
+                            β_type == "βST" ~ "βST - dissim of all intxns as per spp. turnover",
+                            β_type == "βS" ~ "βS - dissim of all spp",
+                            β_type == "βOS" ~ "βOS - dissim of all intxns btwn shared spp."))
 
-ggplot(df,
-       aes(x = extinction_mechanism,
-           y = model)) + 
+
+beta_plot <- ggplot(df,
+                    aes(x = extinction_mechanism,
+                        y = model)) + 
   geom_tile(aes(fill = mean), colour = "grey50") +
-  scale_fill_distiller(palette = "Spectral", direction = 1) +
-  facet_wrap(vars(β_type)) +
+  scale_fill_distiller(palette = "Spectral", direction = -1) +
+  facet_wrap(vars(β_type))  +
+  labs(x = NULL,
+       y = NULL) +
   theme_classic() +
   theme(panel.border = element_rect(colour = 'black',
                                     fill = "#ffffff00"),
         axis.text.x = element_markdown(angle = 45, hjust=1))
 
 ggsave("../figures/beta_div_extinctions.png",
+       beta_plot,
        width = 5000,
        height = 4000,
+       units = "px",
+       dpi = 600)
+
+df <- read_csv("../data/processed/extinction_tss.csv") %>%
+  select(-n_rep) %>%
+  pivot_longer(!c(model, extinction_mechanism)) %>%
+  # get mean (for now might be worth looking at sd as well...)
+  group_by(model, extinction_mechanism, name) %>%
+  summarise(mean = mean(value, na.rm = TRUE))
+
+tss_plot <- ggplot(df,
+                   aes(x = extinction_mechanism,
+                       y = model)) + 
+  geom_tile(aes(fill = mean), colour = "grey50") +
+  scale_fill_distiller(palette = "Spectral", direction = 1) +
+  facet_wrap(vars(name)) +
+  theme_classic() +
+  labs(x = NULL,
+       y = NULL) +
+  theme(panel.border = element_rect(colour = 'black',
+                                    fill = "#ffffff00"),
+        axis.text.x = element_markdown(angle = 45, hjust=1))
+
+ggsave("../figures/tss_extinctions.png",
+       tss_plot,
+       width = 5000,
+       height = 2000,
+       units = "px",
+       dpi = 600)
+
+# combine plots
+layout <- "
+AA
+AA
+BB
+"
+
+(beta_plot +
+    ggtitle('β-diversity') +
+  theme(axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank())) /
+  (tss_plot+
+     ggtitle('TSS')) +
+  plot_layout(design = layout)
+
+ggsave("../figures/turnover_extinctions.png",
+       width = 5000,
+       height = 7000,
        units = "px",
        dpi = 600)
